dbf001a11fef9eac71d5a11b75986bd84c7eb8d7
fakefeik <feikfake1@gmail.com>
2014-11-01 17:26:23 +0500

    Extracted methods

--- CleanCode/01_ExtractMethods.cs
+++ CleanCode/01_ExtractMethods.cs
@@ -1,30 +1,24 @@
 ﻿using System;
 using System.IO;
+using NUnit.Framework.Constraints;
 
 namespace CleanCode
 {
 	public static class RefactorMethod
 	{
+	    public static void SaveToFile(string filename, byte[] data)
+	    {
+	        using (var stream = new FileStream(filename, FileMode.OpenOrCreate))
+	        {
+	            stream.Write(data, 0, data.Length);
+	        }
+	    }
+
 		private static void SaveData(string s, byte[] d)
 		{
-			//open files
-			var fs1 = new FileStream(s, FileMode.OpenOrCreate);
-			var fs2 = new FileStream(Path.ChangeExtension(s, "bkp"), FileMode.OpenOrCreate);
-
-			// write data
-			fs1.Write(d, 0, d.Length);
-			fs2.Write(d, 0, d.Length);
-
-			// close files
-			fs1.Close();
-			fs2.Close();
-
-			// save last-write time
-			string tf = s + ".time";
-			var fs3 = new FileStream(tf, FileMode.OpenOrCreate);
-			var t = BitConverter.GetBytes(DateTime.Now.Ticks);
-			fs3.Write(t, 0, t.Length);
-			fs3.Close();
+		    SaveToFile(s, d);
+		    SaveToFile(Path.ChangeExtension(s, "bkp"), d);
+            SaveToFile(s + ".time", BitConverter.GetBytes((DateTime.Now.Ticks)));
 		}
 	}
 }
